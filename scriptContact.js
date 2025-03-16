2b1ad0c376fcf41f2a09631ba580c48dd112bc51
Vhora Tanvir  Ahmed Yakubbhai <136716771+Tanvir1610@users.noreply.github.com>
2025-03-16 12:37:20 +0530

    Add files via upload

--- scriptContact.js
+++ scriptContact.js
@@ -1,3 +1,5 @@
+const { title } = require("process");
+
 function initMap() {
     var location = { lat: 22.5645, lng: 72.9289 }; // Replace with the actual coordinates
     var map = new google.maps.Map(document.getElementById("map"), {
@@ -7,11 +9,12 @@ function initMap() {
 
     var marker = new google.maps.Marker({
         position: location,
-        map: map
+        map: map,
+        title: "Location"
     });
 
     var infoWindow = new google.maps.InfoWindow({
-        content: '<strong>CVM University Office</strong><br>*It is the location of the University office not the location of the Conference<br><a href="https://maps.app.goo.gl/pR7ovJCzaCmDKg5PA" target="_blank">Google Maps Link</a>'
+        content: '<strong>CVM University Office</strong><br>*It is the location of the Conference (GCET Auditorium) <br><a href="https://maps.app.goo.gl/pR7ovJCzaCmDKg5PA" target="_blank">Google Maps Link</a>'
     });
 
     marker.addListener("click", function () {
